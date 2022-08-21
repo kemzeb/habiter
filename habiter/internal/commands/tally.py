277b8e41c8d368c866913878a47e4708ad7e2d56
Kemal Zebari <zebar002@cougars.csusm.edu>
2022-08-20 22:14:44 -0700

    fix(tally): Update "last_updated" given -z/--zero
    Resolves #3.
    
    - Throw a warning to inform the end user when -z and -n options are
      given at the same time.

--- habiter/internal/commands/tally.py
+++ habiter/internal/commands/tally.py
@@ -3,7 +3,7 @@ import click
 from datetime import datetime
 
 from habiter.internal.utils.consts import HAB_DATE_FORMAT
-from habiter.internal.utils.messenger import echo_failure, echo_success
+from habiter.internal.utils.messenger import echo_failure, echo_success, echo_warning
 from habiter.internal.file.operations import SQLiteDataFileOperations
 
 
@@ -23,19 +23,19 @@ def tally(habits, num, zero):
                            'FROM habit WHERE habit_name=?',
                            (habit_name,))
             row = fo.cur.fetchone()
-
-            if row is not None:
+            if row:
                 if zero:
                     if row['curr_tally'] > 0:
-                        echo_failure(f"Habit \"{habit_name}\" contains occurrences.")
+                        echo_failure(
+                            f'Habit "{habit_name}" already contains occurrences.')
                         flag = True
-                    else:
-                        fo.cur.execute('UPDATE habit SET is_active=? '
-                                       'WHERE habit_id=?',
-                                       (True, row['habit_id']))
-                        echo_success(
-                            f'Habit \"{habit_name}\" marked as active.')
-                    continue
+                        continue
+                    # If a number was passed, make sure to inform the end user
+                    # that the number given will be ignored.
+                    if num != 1:
+                        echo_warning(
+                            "--zero flag passed; ignoring --num value.")
+                    num = 0
 
                 prev_tally = row['curr_tally']
                 curr_tally = row['curr_tally'] + num
@@ -49,10 +49,11 @@ def tally(habits, num, zero):
                                'WHERE habit_id = ?',
                                (curr_tally, total_tally, is_active,
                                 last_updated, prev_tally, row['habit_id']))
-                echo_success("Habit \"{}\" tally updated from {} to {}."
-                             .format(habit_name,
-                                     prev_tally,
-                                     curr_tally))
+                if zero:
+                    echo_success(f'Habit \"{habit_name}\" marked as active.')
+                else:
+                    echo_success(f'Habit "{habit_name}" tally updated '
+                                 f'from {prev_tally} to {curr_tally}.')
             else:
                 echo_failure(f"No habit with the name \"{habit_name}\".")
                 flag = True
