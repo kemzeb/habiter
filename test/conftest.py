c18972357355ce41b388435a101d1c5ec553d4b6
Kemal Zebari <kemalzebra@gmail.com>
2023-04-05 16:43:30 -0700

    test: Create the test DB file once to improve performance
    Running SQLite queries while running tests seem to drastically
    reduce performance of the overall runtime of pytest. One of the
    major areas of blame is making a new DB file and more specifically
    making a create table query with each test case.
    
    This snapshot has the DB file created once, where after each
    test case we remove all records from the database so the next test
    case can work properly. This seems to reduce the overall runtime by
    3 seconds.

--- test/conftest.py
+++ test/conftest.py
@@ -5,14 +5,22 @@ from habiter.internal.file.creator import SQLiteDataFileCreator
 from habiter.internal.file.operations import SQLiteDataFileOperations
 
 
-@pytest.fixture
-def setup(tmpdir) -> None:
-    temp_db_dir_path = tmpdir.mkdir("test")
+@pytest.fixture(scope="session")
+def session_setup(tmp_path_factory) -> None:
+    temp_db_dir_path = tmp_path_factory.mktemp("habiter")
     temp_db_file_name = "test.db"
-    temp_db_file_path = temp_db_dir_path.join(temp_db_file_name)
-    
+    temp_db_file_path = temp_db_dir_path / temp_db_file_name
+
     SQLiteDataFileCreator(temp_db_dir_path, temp_db_file_name).create()
     SQLiteDataFileOperations.set_f_path(temp_db_file_path)
+
+
+@pytest.fixture
+def setup(session_setup) -> None:
+    yield
+
+    with SQLiteDataFileOperations() as fo:
+        fo.cur.execute("DELETE FROM habit")
 
 
 @pytest.fixture
